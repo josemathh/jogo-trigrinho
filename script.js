d7d3aa3ab8b71118df33fd01edf703406c6ba603
josemathh <jose.bacili@escola.pr.gov.br>
2025-08-07 16:30:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function startGame() {
+  alert("🎰 O jogo vai começar! Boa sorte!");
+}
+
+function showBonus() {
+  alert("🎁 Você ganhou 50 giros grátis!");
+}
+
+function openLogin() {
+  alert("🔐 Redirecionando para a página de login...");
+}
+function startGame() {
+  window.location.href = "jogo.html";
+}
